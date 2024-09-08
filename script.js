837040c0575428076d698ef2385ef32c3a6c341e
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2024-09-07 23:53:50 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let sliderElement = document.querySelector("#slider")
-let buttonelement = document.querySelector("button") 
+let buttonelement = document.querySelector("#button") 
 
 let sizePassword = document.querySelector("#valor")
 let Password = document.querySelector("#password")
@@ -7,28 +7,30 @@ let Password = document.querySelector("#password")
 let containerpassword = document.querySelector("#container-password")
 
 let charset = ("abcdefghijkçmnop,qrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678910")
-let novasenha = "";
+let novasenha = ""
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oniput = function() {
+slider.oninput = function() {
     sizePassword.innerHTML = this.value;
 }
 
 function generatepassword () {
   let pass = "";
 
-  for(let i = 0, n = chartset.length; i < sliderElement.value, ++i){
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
     pass += charset.charAt(Math.floor(Math.random() * n));
   }
   console.log(pass)
-}
+
 
 containerpassword.classList.remove("hide");
 Password.innerHTML = pass;
 novasenha = pass;
 
+}
 function copypassword() {
     alert ('senha copiada com sucesso')
     navigator.clipboard.writeText(novasenha);
 }
+
